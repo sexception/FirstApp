9485e2a964fca0ed7cd3e30919e9a556f15ef348
sexception <[sexception@naver.com]>
2018-06-22 15:53:33 +0900

    예제 5장

--- StartApp.js
+++ StartApp.js
@@ -1,10 +1,8 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var express = require('./config/express_config');
+
 var app = express();
-
-/*app.use('/', function(req, res) {
-	res.send('Hello World');
-});*/
-
 app.listen(3000);
 module.exports = app;
 
